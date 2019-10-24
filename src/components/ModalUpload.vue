89acef5eb9c83a5168f5b3f5ef44b2b67a4b9d85
Bonum <alexey.front@bws.pp.ua>
2019-10-24 19:27:45 +0300

    Upload files wip

--- src/components/ModalUpload.vue
+++ src/components/ModalUpload.vue
@@ -1,85 +1,59 @@
 <template>
-    <div class="upload">
-        <input type="file" accept="image/*" @change="uploadImage($event)" id="file-input">
-
-        <div class="upload__list">
-            <div v-for="(item, index) in items" @click="selectItem(item)" :key="index" class="d-flex justify-content-center upload__item">
-                <img :src="item.image" />
-            </div>
-        </div>
+  <div class="upload">
+    <div class="upload__upload-pane">
+      <upload-file />
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                selectedItem: null
-            }
-        },
-        computed: {
-            items() {
-                return this.$store.state.upload.items;
-            }
-        },
-        methods: {
-            selectItem(item) {
-                console.log(item);
-            },
-            uploadImage(event) {
-
-                const URL = 'http://foobar.com/upload';
-
-                let data = new FormData();
-                data.append('name', 'my-picture');
-                data.append('file', event.target.files[0]);
-
-                let config = {
-                    header: {
-                        'Content-Type': 'image/png'
-                    }
-                }
-
-                // axios.put(
-                //   URL,
-                //   data,
-                //   config
-                // ).then(
-                //   response => {
-                //     console.log('image upload response > ', response)
-                //   }
-                // )
-            }
-        }
+import UploadFile from "./UploadFile";
+export default {
+  components: {
+    UploadFile
+  },
+  data() {
+    return {
+      selectedItem: null
     };
+  },
+  computed: {
+    items() {
+      return this.$store.state.upload.items;
+    }
+  },
+  methods: {
+    selectItem(item) {
+      console.log(item);
+    },
+    uploadImage(event) {
+      const URL = "http://foobar.com/upload";
+
+      let data = new FormData();
+      data.append("name", "my-picture");
+      data.append("file", event.target.files[0]);
+
+      let config = {
+        header: {
+          "Content-Type": "image/png"
+        }
+      };
+
+      // axios.put(
+      //   URL,
+      //   data,
+      //   config
+      // ).then(
+      //   response => {
+      //     console.log('image upload response > ', response)
+      //   }
+      // )
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
-    .upload {
-        &__list {
-            display: flex;
-            flex: 1 1 auto;
-            flex-wrap: wrap;
-            width: calc(100% - 337px - 1rem);
-        }
-        &__item {
-            margin: 5px;
-            padding: 5px;
-            border: 1px solid gray;
-            background-color: #f2f2f2;
-            height: 20%;
-            width: 20%;
-
-            &:hover {
-                background-color: #e5e5e5;
-            }
-
-            img {
-                width: 100%;
-                height: 100%;
-            }
-        }
-    }
-
+.upload {
+}
 </style>
---
+++ src/components/UploadFile.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="upload">
+    <b-button squared variant="info" class="upload__btn">
+      <img src="../assets/icons/import.svg" /> Upload an image
+    </b-button>
+    <vue-dropzone
+      ref="myVueDropzone"
+      id="dropzone"
+      :useCustomSlot="true"
+      :options="dropzoneOptions"
+    >
+      <div class="dropzone__content">
+        <img src="../assets/icons/plus.svg" />
+        <p>
+          <b>Upload or drag your image here</b>
+        </p>
+        <p class="dropzone__content-formats">PNG, JPG, GIF, SVG, AI, CDR</p>
+      </div>
+    </vue-dropzone>
+  </div>
+</template>
+
+<script>
+import vue2Dropzone from "vue2-dropzone";
+import "vue2-dropzone/dist/vue2Dropzone.min.css";
+export default {
+  components: {
+    vueDropzone: vue2Dropzone
+  },
+  data() {
+    return {
+      dropzoneOptions: {
+        url: "https://httpbin.org/post",
+        thumbnailWidth: 150,
+        maxFilesize: 0.5,
+        headers: { "My-Awesome-Header": "header value" }
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.upload {
+  width: 296px;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  &__btn {
+    height: 50px;
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    img {
+      margin-right: 10px;
+      max-height: 20px;
+    }
+  }
+  &__dropzone {
+    margin-top: 20px;
+  }
+}
+
+#dropzone {
+  border: dashed 2px lightgrey;
+  margin-top: 20px;
+  height: 100%;
+  .dropzone {
+    &__content {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      color: #8c8c8c;
+      img {
+        width: 30px;
+        margin-bottom: 20px;
+      }
+      &-formats {
+        font-size: 12px;
+      }
+    }
+  }
+}
+</style>
