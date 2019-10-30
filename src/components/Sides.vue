f5db38c0d5a31b541cad90457d4b2c0623632f73
Alexey Konyushkov <alexey.front@bws.pp.ua>
2019-10-30 12:56:05 +0200

    Sides finished

--- src/components/Sides.vue
+++ src/components/Sides.vue
@@ -1,38 +1,56 @@
 <template>
   <div class="sides d-flex justify-content-center">
     <div v-for="(side) in renderSides" :key="side.key" @click="setActiveSide(side)">
-      {{side.title}}
-      <div class="sides__item d-flex flex-column">
-        <svg id="editor" :viewBox="'0 0 500 500'" width="100%" height="100%">
-          <image v-bind:xlink:href="side.image" />
-          <g
-            v-for="(item, index) in side.items"
-            ref="groupEls"
-            :key="index"
-            :transform="'translate('+side.area.x+', '+side.area.y+') rotate('+item.rotate+' '+item.width/2+' '+item.height/2+')'"
-          >
-            <image
-              v-if="item.type=='img'"
-              v-bind:xlink:href="item.file.dataURL"
-              :x="item.x - side.area.x"
-              :y="item.y"
-              :height="item.height"
-              :width="item.width"
+      <div class="sides__item d-flex flex-column" :class="{active: side.key == selectedSide.key }">
+        <svg :viewBox="'0 0 500 500'" width="100%" height="100%">
+          <rect fill="none" width="500" height="500" />
+          <image v-bind:xlink:href="side.image" style="width: 100%" />
+          <g :transform="'translate('+side.area.x+', '+side.area.y+')'">
+            <rect
+              fill="none"
+              vector-effect="non-scaling-stroke"
+              :height="side.area.height"
+              :width="side.area.width"
             />
-            <text
-              v-bind:key="index"
-              v-for="(text, index) in item.text"
-              :x="item.x"
-              :y="item.y"
-              :dy="index + 'em'"
-              :font-family="item.font"
-              :font-size="item.fontSize"
-              :text-anchor="item.textAnchor"
-              :font-weight="item.bold ? 'bold' : 'normal'"
-              :font-style="item.italic ? 'italic' : 'normal'"
-              :fill="item.color"
-              :textLength="item.textAnchor === TextAlignment.JUSTIFIED ? item.width : 0"
-            >{{text}}</text>
+            <g
+              v-for="(item, index) in side.items"
+              ref="groupEls"
+              :key="index"
+              :transform="'translate('+item.x+', '+item.y+') rotate('+item.rotate+' '+item.width/2+' '+item.height/2+')'"
+              width="0"
+            >
+              <rect
+                x="0"
+                y="0"
+                :width="side.area.width"
+                :height="side.area.height"
+                fill="transparent"
+              />
+              <image
+                v-if="item.type=='img'"
+                v-bind:xlink:href="item.file.dataURL"
+                :x="0"
+                :y="0"
+                :height="item.height"
+                :width="item.width"
+              />
+              <text
+                v-bind:key="index"
+                v-for="(text, index) in item.text"
+                :x="0"
+                :y="item.fontSize"
+                :height="item.height"
+                :width="item.width"
+                :dy="index + 'em'"
+                :font-family="item.font"
+                :font-size="item.fontSize"
+                :text-anchor="item.textAnchor"
+                :font-weight="item.bold ? 'bold' : 'normal'"
+                :font-style="item.italic ? 'italic' : 'normal'"
+                :fill="item.color"
+                :textLength="item.textAnchor === TextAlignment.JUSTIFIED ? item.width : 0"
+              >{{text}}</text>
+            </g>
           </g>
         </svg>
       </div>
@@ -42,6 +60,7 @@
 
 <script>
 import { TextAlignment } from "../consts";
+import { mapGetters } from "vuex";
 
 export default {
   data() {
@@ -52,11 +71,11 @@ export default {
   methods: {
     setActiveSide(side) {
       this.$store.commit("setSelectedSide", side.key);
-      this.renderSides();
       this.$store.commit("setSelectedElement", null);
     }
   },
   computed: {
+    ...mapGetters(["selectedSide"]),
     renderSides() {
       const sidesList = this.$store.state.selectedProduct.sides;
       const items = this.$store.state.constructor.items;
@@ -79,16 +98,17 @@ export default {
   padding-right: 10px;
   user-select: none;
   &__item {
-    width: 65px;
-    height: 65px;
-    border: 1px solid grey;
+    width: 100px;
+    height: 100px;
+    border: 1px solid transparent;
     margin: 2px;
-    background-color: #fff;
-    image {
-      width: 100%;
-    }
+    opacity: 0.11;
     &:hover {
       cursor: pointer;
+    }
+    &.active {
+      border: 1px solid #ebebeb;
+      opacity: 1;
     }
   }
 }
