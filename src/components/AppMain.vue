bda0f8b1022626a3385b52802735a5c4fa0cc951
Alexey Konyushkov <alexey.front@bws.pp.ua>
2019-11-20 09:35:55 +0200

    Added spinner to image till it load

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -216,7 +216,41 @@
                     :y="0"
                     :height="item.height"
                     :width="item.width"
+                    @load="item.spinner = false"
                   />
+
+
+                  <svg v-if="item.type=='img' && item.spinner == true" :x="(item.width/2 - 20)" :y="(item.height/2 - 20)" width="40" height="40" viewBox="0 0 38 38" xmlns="http://www.w3.org/2000/svg">
+                    <defs>
+                        <linearGradient x1="8.042%" y1="0%" x2="65.682%" y2="23.865%" id="a">
+                            <stop stop-color="#72b425" stop-opacity="0" offset="0%"/>
+                            <stop stop-color="#72b425" stop-opacity=".631" offset="63.146%"/>
+                            <stop stop-color="#72b425" offset="100%"/>
+                        </linearGradient>
+                    </defs>
+                    <g fill="none" fill-rule="evenodd">
+                        <g transform="translate(1 1)">
+                            <path d="M36 18c0-9.94-8.06-18-18-18" id="Oval-2" stroke="url(#a)" stroke-width="2">
+                                <animateTransform
+                                    attributeName="transform"
+                                    type="rotate"
+                                    from="0 18 18"
+                                    to="360 18 18"
+                                    dur="0.9s"
+                                    repeatCount="indefinite" />
+                            </path>
+                            <circle fill="#fff" cx="36" cy="18" r="1">
+                                <animateTransform
+                                    attributeName="transform"
+                                    type="rotate"
+                                    from="0 18 18"
+                                    to="360 18 18"
+                                    dur="0.9s"
+                                    repeatCount="indefinite" />
+                            </circle>
+                        </g>
+                    </g>
+                </svg>
                 </svg>
                 <g v-if="selectedElement === item && !selectedLayers.length">
                   <rect
@@ -1117,6 +1151,7 @@ export default {
                   y: 0
               },
               selected: false,
+              spinner: true
           };
       },
       updateSizes() {        
