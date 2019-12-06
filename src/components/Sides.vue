0d5030c3b14d1a3ae3c8405ff8e72a76bbc15c94
Alexey Konyushkov <alexey.front@bws.pp.ua>
2019-12-06 21:13:15 +0200

    Mainblank removed from /tocart

--- src/components/Sides.vue
+++ src/components/Sides.vue
@@ -5,7 +5,14 @@
         class="sides__item d-flex flex-column"
         :class="{active: side.id == active(), invalid: side.invalid && maxPrintSize }"
       >
-        <svg :viewBox="'0 0 500 500'" width="500" height="500">
+        <svg
+          :viewBox="'0 0 500 500'"
+          width="500"
+          height="500"
+          xmlns="http://www.w3.org/2000/svg"
+          version="1.1"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+        >
           <defs>
             <mask id="mainMask" v-html="side.area.svg_area" maskUnits="userSpaceOnUse" />
           </defs>
@@ -19,7 +26,13 @@
                 mask="url(#mainMask)"
               >
                 <g :transform="item.matrix ? item.matrix : ''">
-                  <svg :height="500" :width="500" :x="0" :y="0" :opacity="base.layers_opacity">
+                  <svg
+                    :height="item.height"
+                    :width="item.width"
+                    :x="item.x"
+                    :y="item.y"
+                    :opacity="base.layers_opacity"
+                  >
                     <image
                       v-if="item.type=='img'"
                       :xlink:href="item.url ? imgUrl(item.url) : item.dataUrl"
@@ -31,8 +44,8 @@
                     <text
                       :height="item.height"
                       :width="item.width"
-                      :x="item.x"
-                      :y="item.y"
+                      :x="0"
+                      :y="item.height/item.text.length"
                       v-bind:key="index"
                       v-for="(text, index) in item.text"
                       :dy="index + 'em'"
---
+++ src/utils/renderSvg.js
@@ -0,0 +1,112 @@
+import { CONSTRUCTOR_ADD_ITEM } from '../store/mutations.type';
+
+export default function renderSvg(context, svg, side) {   
+    let element = new DOMParser().parseFromString(svg, "text/html");
+    let groups = element.getElementById("containerGroupMain");  
+  
+    let images = groups.querySelectorAll('image');
+    let texts = groups.querySelectorAll('text');
+   
+    if(images) {
+        createImageLayers(context, images, side)
+    }
+   
+    if(texts) {
+        createTextLayers(context, texts, side)
+    }
+   
+}
+
+function createImageLayers(context, arr, side) {
+    let layer = {}
+    let edArea = document.getElementById('editable-area');
+    arr.forEach(item => {
+        layer = {
+            width: +item.getAttribute('width'),
+            height: +item.getAttribute('height'),
+            x: +item.parentNode.getAttribute('x'), 
+            y: +item.parentNode.getAttribute('y'),
+            selected: false,
+            spinner: true,
+            type: "img",   
+            node: null,
+            name: item.dataset && item.dataset.layername ? item.dataset.layername : '',
+            o: {
+                x: 0,
+                y: 0
+            },
+            side: side,
+            sideName: "Перед",
+            rotate: 0,
+        }
+        if(item.parentNode.parentNode.getAttribute('transform') && item.parentNode.parentNode.getAttribute('transform').includes('matrix')) {          
+            layer.matrix = item.parentNode.parentNode.getAttribute('transform');
+            const matrix = item.parentNode.parentNode.getAttribute('transform').match(/(-?\d{1,}\.?\d?){1,}/g)
+            let skewX = +matrix[1] || 0;
+            layer.rotate = Math.asin(skewX/Math.PI)*180;
+        }
+        let url = item.getAttribute('xlink:href')
+        if(url && url.includes('base64')) {
+            layer.dataUrl = item.getAttribute('xlink:href');
+        } else {
+            layer.url = item.getAttribute('xlink:href');
+        }      
+        context.commit(CONSTRUCTOR_ADD_ITEM, {...layer})
+       
+    })
+}
+
+function createTextLayers(context, arr, side) {
+    let layer = {}
+    let edArea = document.getElementById('editable-area');   
+    arr.forEach(item => {
+        layer = {
+            width: +item.parentNode.getAttribute('width'),
+            height: +item.parentNode.getAttribute('height'),
+            x: +item.parentNode.getAttribute('x'), 
+            y: +item.parentNode.getAttribute('y'),
+            selected: false,
+            type: "text",   
+            node: null,
+            name: item.dataset && item.dataset.layername ? item.dataset.layername : '',
+            o: {
+                x: 0,
+                y: 0
+            },
+            side: side,
+            sideName: "Перед",
+            textAnchor: "start",            
+            text: [],       
+            font: { name: item.getAttribute('font-family')},          
+            color: item.getAttribute('fill') || '#000',
+            bold: item.getAttribute('font-style') == 'bold' ? true : false, 
+            italic: item.getAttribute('font-style') == 'italic' ? true : false, 
+            rotate: 0,    
+        }        
+        if(item.children.length) {           
+            Array.from(item.children).forEach(tSpan => {
+                layer.text.push(tSpan.textContent)
+            })
+            layer.fontSize = layer.height / layer.text.length;
+        } else {
+            layer.text.push(item.textContent)
+            layer.fontSize = layer.height / layer.text.length;
+        }
+       
+        if(item.parentNode.parentNode.getAttribute('transform') && item.parentNode.parentNode.getAttribute('transform') && item.parentNode.parentNode.getAttribute('transform').includes('matrix')) {            
+            layer.matrix = item.parentNode.parentNode.getAttribute('transform');
+            const matrix = item.parentNode.parentNode.getAttribute('transform').match(/(-?\d{1,}\.?\d?){1,}/g)
+
+            let skewX = +matrix[1] || 0;
+            layer.rotate = Math.asin(skewX/Math.PI)*180;
+            // let x = layer.x;
+            // let y = layer.y;
+                          
+            // layer.width *= +matrix[0]
+            // layer.x = item.parentNode.getAttribute('x')*+matrix[0] - item.parentNode.getAttribute('x')*+matrix[1] + +matrix[4] - +edArea.getAttribute('x');
+            // layer.y = item.parentNode.getAttribute('y')*+matrix[3] - item.parentNode.getAttribute('y')*+matrix[2] + +matrix[5] - +edArea.getAttribute('y'); 
+        }
+        context.commit(CONSTRUCTOR_ADD_ITEM, {...layer})
+       
+    })
+}
