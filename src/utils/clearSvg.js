da00c2b15568342fa14691e0f2ff16acd43f5fbe
Alexey Konyushkov <alexey.front@bws.pp.ua>
2020-06-10 13:51:54 +0300

    Fixed send svg (freeze on save)

---
+++ src/utils/clearSvg.js
@@ -0,0 +1,34 @@
+export default function clearSvg(str) { 
+    console.log('CLEAR');
+    let elementDom = new DOMParser().parseFromString(
+      str,
+      "text/html"
+    );
+    // svgItem.svg = svgItem.svg
+            //   .replace(/.([^<]*)mainblanks(.*?)<\/image>/, "")
+            //   .replace(/<mask.*mask>/, "")
+            //   .replace(/mask="url\(#mainMask\)"/g, "");
+            
+    let svg = elementDom.querySelector('svg');
+
+    let maskEl = svg.querySelector('mask');
+    if(maskEl) {
+        maskEl.remove();
+    }
+   
+    
+    let mainImgEl = svg.querySelector('image');
+    if(mainImgEl) {
+        mainImgEl.remove();
+    }
+   
+
+    let elementsMask = svg.querySelectorAll('[mask]');
+    if(elementsMask) {
+        elementsMask.forEach(node => {
+            node.removeAttribute('mask')
+        })
+    }   
+    
+    return svg.parentElement.innerHTML;
+} 
