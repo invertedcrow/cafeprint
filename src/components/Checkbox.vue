aaa47b44a4e854af6824606ea05f9d48cf269187
Alexey Konyushkov <alexey.front@bws.pp.ua>
2020-07-22 16:41:48 +0300

    CSS class name changed

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="custom-checkbox" :class="{checked: checked}">
+  <div class="constructor-custom-checkbox" :class="{checked: checked}">
     <svg
       v-if="checked"
       xmlns="http://www.w3.org/2000/svg"
@@ -22,6 +22,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.constructor-custom-checkbox {
+    width: 21px;
+    height: 21px;
+    border-radius: 4px;
+    border: 1px solid #a2a2a2;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-shrink: 0;
+    img {
+        width: 80%;
+        height: 80%;
+        display: none;
+    }
+    &.checked {
+        background-color: #72B425;
+        img {
+            display: block;
+        }
+
+    }
+}
+
 svg {
   width: 14px;
   height: 14px;
